9115507a38d5d53d5df7a0995074564c8e946dcb
SibilaA <110829661+SibilaA@users.noreply.github.com>
2023-04-17 14:04:42 -0300

    Update Shelve.cs

--- Ejercicio 1/src/Library/Shelve.cs
+++ Ejercicio 1/src/Library/Shelve.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 
+/*
+El código ahora cumple con el princpio SRP ya que cada clase tiene una responsabilidad única. 
+Libro se encarga de los libros, Shelve de los estantes y Sector de los sectores. 
+Todo está encapsulado, de esta forma los libros se crean en la Clase Libro, luego se guardan en una estantería en la clase Shelve.
+Por último, las estanterías son puestas en sectores en la clase Sector. 
+*/
+
 namespace SRP
 {
     public class Shelve
